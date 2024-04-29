b8186473b09cba1395d8bf25d63171f9c257e988
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2024-04-29 13:08:10 +0900

    feat: useTagsQuery.ts에서 tag를 오름차순 정렬기능 추가 (#10)
    * feat: useTagsQuery.ts에서 tag를 오름차순 정렬기능 추가
    
    * bug: 타입 오류 제거

--- src/hooks/useTagsQuery.ts
+++ src/hooks/useTagsQuery.ts
@@ -1,9 +1,23 @@
 import usePostsQuery from "./usePostsQuery"
 import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
 
-export const useTagsQuery = () => {
-  const posts = usePostsQuery()
-  const tags = getAllSelectItemsFromPosts("tags", posts)
-
-  return tags
+interface ItemCount {
+  [itemName: string]: number;
 }
+
+
+export const useTagsQuery = (): ItemCount => {
+  const posts = usePostsQuery();
+  const tags = getAllSelectItemsFromPosts("tags", posts);
+
+  return Object.entries(tags)
+    .sort(([a], [b]) => {
+      if (a === "방명록") return -1;
+      if (b === "방명록") return 1;
+      return a.localeCompare(b);
+    })
+    .reduce((obj, [key, value]) => {
+      obj[key] = value;
+      return obj;
+    }, {} as ItemCount);
+};
